550167c8c24c9d2ddc2cf6f9dcd26da571f358e5
yubaibai15 <439964464@qq.com>
2026-08-30 19:33:30 +0800

    Add files via upload

---
+++ visual-skills.js
@@ -0,0 +1,30 @@
+(() => {
+  const visualSkills = [
+    '\u89c6\u9891\u5206\u955c\u751f\u6210',
+    '\u56fe\u7247\u751f\u6210',
+    '\u89c6\u9891\u751f\u6210'
+  ];
+
+  const isVisualDesigner = () => chatAgent?.role === '\u89c6\u89c9\u8bbe\u8ba1\u5e08';
+  const installSkills = () => {
+    if (!isVisualDesigner()) return;
+    const picker = document.querySelector('#agent-chat .skill-picker');
+    if (!picker || picker.dataset.visualSkillsInstalled === 'true') return;
+    picker.dataset.visualSkillsInstalled = 'true';
+    visualSkills.forEach((name, index) => {
+      const button = document.createElement('button');
+      button.type = 'button';
+      button.innerHTML = `<i>SK${index + 5}</i><b>${name}</b><span>\u8c03\u7528</span>`;
+      button.addEventListener('click', () => window.runComposerSkill?.(name));
+      picker.appendChild(button);
+    });
+  };
+
+  const baseOpenAgentChat = window.openAgentChat;
+  window.openAgentChat = id => {
+    baseOpenAgentChat(id);
+    installSkills();
+  };
+  window.openAgent = window.openAgentChat;
+  window.setTimeout(installSkills, 0);
+})();
